e8700b09cfdc08a8b20ef31df6050e1233f80999
Jonathan Yu <jonyu02@gmail.com>
2021-06-17 15:19:21 -0400

    commit tree 08da4df03f37f0f304cc6c5b6055ace968a856ba

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,41 @@
+
 class Instructor
+
+    attr_accessor :name
+
+    @@all= []
+
+    def initialize(name)
+        
+        @name = name
+        @@all << self
+
+    end
+
+    def self.all
+
+        @@all
+
+    end
+    
+    def pass_student(student, test_name)
+
+        pass = BoatingTest.all.find{|test| test.student.first_name == student.first_name && test.name == test_name}
+        if pass
+            pass.status= "passed"
+        else
+            BoatingTest.new(student, test_name, "passed", self)
+        end
+
+    end
+
+    def fail_student(student, test_name)
+        fail = BoatingTest.all.find{|test| test.student.first_name == student.first_name && test.name == test_name}
+        if fail
+            fail.status= "failed"
+        else
+            BoatingTest.new(student, test_name, "failed", self)
+        end
+    end
 
 end
